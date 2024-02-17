e23e87c010a04bc7808c2578bf234afcdd2c759d
dineshkumar7799 <kadirisanidinesh1205@gmail.com>
2024-02-17 20:43:57 +0530

    changes arec done

--- name.c
+++ name.c
@@ -5,6 +5,8 @@ int main(){
 	printf("Hello World..!");
 	printf("1234.....10");
 	printf("oiuytrewertyu");
+	printf("hello all ia ma mdbjbjbdj");
+	printf(",mndsnbfsdbjcbjsdncki");
 	printf("hello i am dinesh kumar");
 return 0;
 }
